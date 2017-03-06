be7026b29627921aa058a1a1b741ff199bff6b86
ShlokD <shlokpdesai@gmail.com>
2017-03-05 18:43:34 -0800

    Sum of even fibonacci

--- src/projecteuler/ProblemSetsTest.java
+++ src/projecteuler/ProblemSetsTest.java
@@ -12,5 +12,11 @@ public class ProblemSetsTest {
 		long expectedSum = 233168;
 		assertEquals(expectedSum, problemSets.sumThreeFiveMultiples(1000));
 	}
-
+	
+	@Test
+	public void evenFibonacciTest() {
+		ProblemSets problemSets =  new ProblemSets();
+		long expectedSum = 7;
+		assertEquals(expectedSum, problemSets.evenFibonacciNumbers(4));
+	}
 }
